f2096fa1fb5434a158f036dc5f9ad007d50a2a6e
Kinyua <alfkinyua@gmail.com>
2022-06-14 09:17:24 +0300

    Add Javascript functionality

---
+++ style.js
@@ -0,0 +1,20 @@
+const ham = document.querySelector('.hamburger');// select the humberger icon 
+const closeNow = document.querySelector('.closed'); // select the close image 
+const menu_Items = document.querySelectorAll(".mobile-menu") //select all menu items 
+
+function displayMenuNav() {
+    document.querySelector('.mobile-container').classList.add('active');
+    ham.classList.add('invisible');
+   
+} 
+ham.addEventListener('click', displayMenuNav) // create an event listener to activate the displayNav function
+
+function closeNav(){
+    document.querySelector('.mobile-container').classList.remove('active');
+    ham.classList.remove('invisible');  
+} 
+closeNow.addEventListener('click', closeNav) //closes the menu nav section
+
+menu_Items.forEach((item) => {
+    item.addEventListener('click', closeNav) //closes the menu nav section whenever any menu item is clicked 
+});
